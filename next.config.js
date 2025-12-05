8ba8f7b42da569522bb8fc789ef6f76ffd120bbb
Mayn <maynrephoneone@gmail.com>
2025-12-05 18:52:28 +0100

    Fix: enable Turbopack and keep @ alias in Next.js config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {}, // aktiviert Turbopack Build, verhindert Fehler bei Vercel
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
@@ -9,3 +10,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
